96be0743139d09223e18fe5ddb0db2bce0a07365
Jacob-0810 <jacobourbano0810@gmail.com>
2025-04-29 12:24:12 -0500

    Add blocks.js 1745947451955

---
+++ blocks.js
@@ -0,0 +1,29 @@
+export const candidateBlocks = [
+  {
+    id: 'block1',
+    name: 'Bloque A',
+    proposals: [
+      'Propuesta 1: Mejorar la biblioteca',
+      'Propuesta 2: Más actividades deportivas',
+      'Propuesta 3: Mejorar el comedor escolar'
+    ]
+  },
+  {
+    id: 'block2',
+    name: 'Bloque B',
+    proposals: [
+      'Propuesta 1: Más salidas culturales',
+      'Propuesta 2: Renovación de laboratorios',
+      'Propuesta 3: Mejorar el transporte escolar'
+    ]
+  },
+  {
+    id: 'block3',
+    name: 'Bloque C',
+    proposals: [
+      'Propuesta 1: Implementar reciclaje',
+      'Propuesta 2: Más talleres artísticos',
+      'Propuesta 3: Mejorar la seguridad'
+    ]
+  }
+];
